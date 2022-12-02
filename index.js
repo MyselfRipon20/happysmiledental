6ec842a98ded231d6ce170e30ac84832123401d8
Sk Ripon <93174805+MyselfRipon20@users.noreply.github.com>
2022-12-02 11:41:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+let openMap = () => {
+    window.location.assign("https://www.google.com/maps/place/Tahaj+Medical+Hall/@23.2188081,88.3500675,19.77z/data=!4m5!3m4!1s0x39f8e51a792abdc5:0xddc2842e1ef25816!8m2!3d23.2187196!4d88.3503924")
+}
